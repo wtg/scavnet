56f720e3462d313a351bec5cd7917a1bc88606ba
Brian M <michab3@rpi.edu>
2009-10-24 13:44:51 -0400

    Sort locations & categories by name on default

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :items
+  
+  #Sort by name on default
+  default_scope :order => 'name ASC'
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ActiveRecord::Base
   has_many :items
+  
+  #Sort by name on default
+  default_scope :order => 'name ASC'
 end
