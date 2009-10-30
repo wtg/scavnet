84ca706361d36363ee2c5dbac73b5072316096f1
mikldt <ditorm@rpi.edu>
2009-10-30 00:18:38 -0400

    More work with authentication. esp. LDAP.
    Updated authenticates_rpi to do magical things with LDAP for new users.
    Note that config/initializers/ldap_intializer.rb is required for this to
    work (there is a sample in that directory), as is the ruby-ldap plugin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,15 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
+  # Set up authentication.
+  # In this application, LDAP information may be provided in an initializer
+  # (see config/initializers/ldap_intializer.rb.sample). It may also be skipped
+  # if not using LDAP. CAS information should be provided in an intializer or
+  # environment.rb (see config/initializers/cas_initializer.rb.sample) or 
+  # ruby-cas documentaiton.
   authenticate_rpi User, :username_field => 'rcsid', 
-    :admin_field => 'is_admin', :autoadd_users => true
+    :admin_field => 'is_admin', :autoadd_users => true,
+    :fullname_field => 'name',
+    :ldap_address => LDAP_ADDRESS, :ldap_port => LDAP_PORT,
+    :ldap_dn => LDAP_DN
 end
